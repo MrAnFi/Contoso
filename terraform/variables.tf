884b62f64fc8e1cda69ec389555ec6b7ae44f525
Antonin Fiurasek <antonin@fiurasek.eu>
2024-07-06 20:34:32 +0200

    0.0.1

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+  type        = string
+  description = "name of the app"
+  default     = "Contoso"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Resources"
+  default     = "westeurope"
+}
